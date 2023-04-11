79a74823c8335c5d8724cb4f60b72249f0b8c11f
Gabriel <gabrielcfacul@gmail.com>
2023-04-11 16:55:33 -0300

    v1.1

--- src/routes/carRoute.js
+++ src/routes/carRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/carController");
 
-router.get("/", controller.get); //http://localhost:3000
+router.get("/", controller.get); //http://localhost:3000 -> Detalhes da API
 router.get("/model", controller.getByModel); //http://localhost:3000/cars/model?model=HR-V&month=Março
 router.get("/brand", controller.getByBrand); //http://localhost:3000/cars/brand?brand=GM&month=Fevereiro
 router.get("/position", controller.getByPosition); //http://localhost:3000/cars/position?position=1&month=Março
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -4,8 +4,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.status(200).send({
     title: "API EXA844 Project",
-    version: "1.0",
-    type: req.method,
+    version: "1.1",
   });
 });
 
