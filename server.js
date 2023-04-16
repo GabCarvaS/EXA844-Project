6627fb3525b3c6d8d013e5679f9a86b1dbdaa864
Gabriel <gabrielcfacul@gmail.com>
2023-04-16 18:57:58 -0300

    atualizando dependencias e criando readme

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const http = require("http");
 const debug = require("debug")("nodestr:server");
 const express = require("express");
-
 const app = express();
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "8000");
 app.set("port", port);
 
 const server = http.createServer(app);
